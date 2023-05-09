ea4177cebd76b7f8571e090c760d4b8ee3fec1a4
Gordei <gordeyvasilev@gmail.com>
2023-05-09 17:29:25 +1000

    final

---
+++ 10 Deep dive into OOP. Part 1/UnitTest11.cs
@@ -0,0 +1,15 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System;
+
+//namespace Homework_Theme_10
+//{
+//    [TestClass]
+//    public class UnitTest11
+//    {
+//        [TestMethod]
+//        public void MyTestMethod()
+//        {
+
+//        }
+//    }
+//}
