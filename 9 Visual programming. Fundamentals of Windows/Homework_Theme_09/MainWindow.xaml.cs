2f05016ad64a83abe1dd8af73f00de3c7d9470a5
Gordei <gordeyvasilev@gmail.com>
2023-05-09 17:00:39 +1000

    Start

---
+++ 9 Visual programming. Fundamentals of Windows/Homework_Theme_09/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework_Theme_09
+{
+    public partial class MainWindow : Window
+    {
+        private readonly Utils utils;
+
+        public MainWindow()
+        {
+            utils = new Utils(this);
+            InitializeComponent();
+        }
+
+        private void BtnSplitSentenceClick(object sender, RoutedEventArgs e)
+        {
+            listOutput.ItemsSource = utils.SplitSentence(txtInput.Text);
+        }
+
+        private void BtnReverseWordsClick(object sender, RoutedEventArgs e)
+        {
+            txtOutput.Text = utils.ReverseWords(txtInput.Text);
+        }
+    }
+}
---
+++ 9 Visual programming. Fundamentals of Windows/Homework_Theme_09/Utils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Homework_Theme_09
+{
+    internal class Utils
+    {
+        private readonly MainWindow mainWindow;
+
+
+        public Utils(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+
+        public List<string> SplitSentence(string sentence)
+        {
+            return sentence.Split(' ').ToList();
+        }
+
+        public string ReverseWords(string sentence)
+        {
+            return String.Join(" ", sentence.Split(' ').Reverse());
+        }
+    }
+}
