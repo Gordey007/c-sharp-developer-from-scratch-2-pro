6bd30a77e1dc3041279906393cbcec83573777ec
Gordei <gordeyvasilev@gmail.com>
2023-05-09 17:13:38 +1000

    10

---
+++ 10 Deep dive into OOP. Part 1/Manager.cs
@@ -0,0 +1,26 @@
+﻿namespace Homework_Theme_10
+{
+    internal class Manager : Employee, IEditing
+    {
+        /// <summary>
+        /// Менеджер
+        /// </summary>
+        /// <param name="EmployeeType">Тип сотрудника</param>
+        public Manager(string EmployeeType = "Manager") : base(EmployeeType) { }
+
+
+        /// <summary>
+        /// Приветствие
+        /// </summary>
+        public override void Authorization() { Console.WriteLine("Хорошего рабочего дня менеджер!"); }
+
+        /// <summary>
+        /// Информация о прав доступа
+        /// </summary>
+        public void GetInfoAccess()
+        {
+            Console.WriteLine("Менеджер может изменять: фамилию; имя; отчество; " +
+                "серию и номер паспорта; номер телефона клиента");
+        }
+    }
+}
