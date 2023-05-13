b7e6d0bed80aa2924b67b0c727a0b3ef65912eb4
Gordei <gordeyvasilev@gmail.com>
2023-05-13 13:43:59 +1000

    Added Binding and using INotifyPropertyChanged

--- 9 Visual programming. Fundamentals of Windows/Homework_Theme_09/MainWindow.xaml.cs
+++ 9 Visual programming. Fundamentals of Windows/Homework_Theme_09/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Homework_Theme_09
 {
@@ -21,7 +8,7 @@ namespace Homework_Theme_09
 
         public MainWindow()
         {
-            utils = new Utils(this);
+            utils = new Utils();
             InitializeComponent();
         }
 
@@ -32,7 +19,8 @@ namespace Homework_Theme_09
 
         private void BtnReverseWordsClick(object sender, RoutedEventArgs e)
         {
-            txtOutput.Text = utils.ReverseWords(txtInput.Text);
+            Reverse reverseStr = (Reverse)this.Resources["reverseStrDefault"];
+            reverseStr.ReverseStr = utils.ReverseWords(txtInput.Text);
         }
     }
 }
---
+++ 9 Visual programming. Fundamentals of Windows/Homework_Theme_09/Reverse.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Homework_Theme_09
+{
+    internal class Reverse : INotifyPropertyChanged
+    {
+        private string reverseStr;
+
+        public string ReverseStr
+        {
+            get { return reverseStr; }
+            set
+            {
+                reverseStr = value;
+                OnPropertyChanged("ReverseStr");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
